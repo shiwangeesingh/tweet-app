d3cf16671126a72594bc0381d08e705b85ef3bdd
shiwangeesingh <shiwangeesingh0@gmail.com>
2020-05-25 19:07:22 +0530

    update

--- dbConnection.js
+++
@@ -1,4 +0,0 @@
-const mongoose = require('mongoose');
-const conn = mongoose.createConnection("mongodb://127.0.0.1:27017/tweet-app");;
-exports.mongoose = mongoose;
-exports.conn = conn;
--- index.js
+++ index.js
@@ -7,8 +7,7 @@ created by me, view the post you are taged on
         
 const express = require('express');
 const app = express();
-const { UserModel } = require("./model/user");
-const { PostModel } = require("./model/post");
+const fs = require('fs')
 const bodyParser = require ('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
@@ -16,118 +15,215 @@ let server = app.listen((3000));
 
 
  //************************************* signup ************************** */
-  // please pass in body -: name,password
+  // please pass in body -: userName
   app.post('/signup', async(req,res)=>{
     try{
-    if(!req.body.name || !req.body.password) 
-    throw new Error("Some parameter are missing!") 
-    const checkUser = await UserModel.findOne({name:req.body.name})
-    if(checkUser)
-    throw new Error("User already registered")
-    let tagName = "@"+req.body.name;
-    const data = await UserModel.create({name:req.body.name, password: req.body.password,tagName})
-    if(!data)
-    throw new Error("Unable to save data!")
-    res.status(200).json({response:data, message:"Signup successful!"});
+    if(!req.body.userName) 
+    throw new Error("userName is required!") 
+    let tagName = "@"+req.body.userName;
+    fs.readFile('user.json', 'utf8', function readFileCallback(err, userList){
+        if (err){
+            console.log(err);
+        } else {
+        if(userList)
+        obj = JSON.parse(userList);
+        else
+        obj = []
+        if(obj.length){
+            if(userList.includes(tagName)){
+                res.status(403).json({error:"User already registered"});
+                return
+            }
+        }            
+        obj.push(tagName)
+        json = JSON.stringify(obj); 
+        fs.writeFile('user.json', json, 'utf8',(err)=>{
+            if (err){
+                console.log(err);
+            } else {
+                res.status(200).json({response:{name:tagName}, message:"Signup successful!"});
+            }
+        })}
+    });
 }
     catch(error){
         res.status(403).json({error:error.message});
     }
 });
 
-//************************************* Login ************************** */
-  // please pass in body -: name,password
-app.post('/login', async(req,res)=>{
-    try{
-    const data = await UserModel.findOne({name:req.body.name, password: req.body.password},{password:0})
-    if(!data)
-    throw new Error("Invalid credentials!")
-    res.status(200).json({response:data, message:"Login successful!"});
-    }
-  catch(error){
-        res.status(403).json({error:error.message});
-    }  
-})
+
 
 //************************************* Post ************************** */
-  // please pass in body -: post,userId-your _id , tagedUser: array of userId of the users whom you want to tag
+  // please pass in body -: post,userName-your userName , tagedUser: array of @userName of the users whom you want to tag
 app.post('/post', async(req,res)=>{
     try{
-    const checkUser = await UserModel.findOne({_id:req.body.userId})
-    if(!checkUser)
-    throw new Error("Invalid User!")
-    if(!req.body.post)
-    throw new Error("Please enter valid post!")
-    if(!req.body.tagedUser)
-    throw new Error("Please tag atleast one user")
-    if(req.body.tagedUser.length == 0)
-    throw new Error("Please tag atleast one user")
-    let createPost = await PostModel.create({post:req.body.post,userId:req.body.userId,tagedUser:req.body.tagedUser})
-    if(!createPost)
-    throw new Error("Unable to save data!")
-    res.status(200).json({response:createPost, message:"Post successfully uploaded!"});
-    }
+        if(!req.body.userName)
+        throw new Error("Please send user name!")
+
+        let tagedUser = req.body.tagedUser
+        fs.readFile('user.json', 'utf8', function readFileCallback(err, userList){
+            if (err){
+                console.log(err);
+            } else {
+            if(userList)
+            obj = JSON.parse(userList);
+            console.log("userList",userList)
+                if(!userList.includes(req.body.userName) && !userList.includes('@'+req.body.userName)){
+                    res.status(403).json({error:"Inavalid user!"});
+                    return
+                }
+                for(let i =0; i<tagedUser.length;i++){
+                    if(!userList.includes(tagedUser[i]) && !userList.includes('@'+tagedUser[i])){
+                        res.status(403).json({error:"Invalid taged user!"});
+                        return
+                    }
+                }
+                if(!req.body.post)
+                throw new Error("Please enter valid post!")
+                if(!req.body.tagedUser)
+                throw new Error("Please tag atleast one user")
+                if(req.body.tagedUser.length == 0)
+                throw new Error("Please tag atleast one user")
+                let data = {post:req.body.post,userName:req.body.userName,tagedUser:req.body.tagedUser}
+                var postList = []
+                fs.readFile('post.json', 'utf8', function readFileCallback(err, data1){
+                    if (err){
+                        console.log(err);
+                    } else {
+                        
+                    if(data1){
+                        obj = JSON.parse(data1);
+                        obj.push(data)
+                        json = JSON.stringify(obj);
+                     } 
+                     else{
+                         postList.push(data)
+                         json = JSON.stringify(postList);
+                     }
+                     fs.writeFile('post.json', json, 'utf8',(err)=>{
+                        if (err){
+                            console.log(err);
+                        } else {
+                            res.status(200).json({response:data, message:"Post created successfully!"});
+            
+                        }
+                    })
+                }
+            });
+        }
+    });
+}
   catch(error){
         res.status(403).json({error:error.message});
     }  
 })
 
 //***************************** Search @user *************************** */
-  // please pass in body -: search: key you want to search along with @ (eg. @yourName),userId-your _id 
+  // please pass in body -: search: key you want to search along with @ (eg. @yourName),
 app.post('/searchUser', async(req,res)=>{
-    try{
-    if(!req.body.userId)
-    throw new Error("Please send your _id!")   
-    if(!req.body.search)
+    try{ 
+        let search = req.body.search 
+    if(!search)
     throw new Error("Please enter name!")
-    const userList = await UserModel.find({
-            $nor:[{
-                _id:req.body.userId
-            }],
-            tagName: {
-                $regex: req.body.search,
-                $options: 'i'
+    fs.readFile('user.json', 'utf8', function readFileCallback(err, userList){
+        if (err){
+            console.log(err);
+        } else {
+        if(userList)
+        obj = JSON.parse(userList);
+        console.log("userList",userList)
+            if(userList.includes(search) || userList.includes("@"+search)){
+                res.status(200).json({response:{name:search}});
+                return
             }
-    },{
-        tagName:1
-    })
-    res.status(200).json({response:userList});
-    }
-  catch(error){
-        res.status(403).json({error:error.message});
-    }  
+             else {
+                res.status(403).json({message:"user not found!"});
+
+            }
+        }
+    });
+}
+    catch(error){
+            res.status(403).json({error:error.message});
+        }  
 })
 
 //************************************* Post ************************** */
 // body- userId:your userId
 app.post('/myTweet', async(req,res)=>{
     try{
-    if(!req.body.userId)
-    throw new Error("Please send your userId!")      
-    const checkUser = await UserModel.findOne({_id:req.body.userId})
-    if(!checkUser)
-    throw new Error("Invalid User!")
-    let postList = await PostModel.find({}).sort({createdAt:-1})
-    res.status(200).json({response:postList});
-    }
-  catch(error){
+    var myPost = []
+    if(!req.body.userName)
+    throw new Error("Please send your user name!") 
+    fs.readFile('user.json', 'utf8', function readFileCallback(err, userList){
+        if (err){
+            console.log(err);
+        } else {
+        if(userList)
+        obj = JSON.parse(userList);
+        console.log("userList",userList)
+            if(!obj.includes(req.body.userName) && !obj.includes("@"+req.body.userName)){
+                res.status(403).json({error:"Invalid user!"});
+                return
+            }
+            else{    
+                fs.readFile('post.json', 'utf8', function readFileCallback(err, postList){
+                    if (err){
+                        console.log(err);
+                    } else {
+                    if(postList)
+                    objPost = JSON.parse(postList);
+                    for( let list of objPost){
+                        if(list.userName == req.body.userName)
+                        myPost.push(list)
+                    }
+                    res.status(200).json({response:myPost});
+                }}); 
+            }            
+        }
+    }); 
+}
+    catch(error){
         res.status(403).json({error:error.message});
     }  
 })
 
 //************************************* Post ************************** */
-// body- userId:your userId
+// body- userName:your userName
 app.post('/myMentions', async(req,res)=>{
     try{
-    if(!req.body.userId)
-    throw new Error("Please send your userId!")      
-    const checkUser = await UserModel.findOne({_id:req.body.userId})
-    if(!checkUser)
-    throw new Error("Invalid User!")
-    let postList = await PostModel.find({tagedUser:{$in:req.body.userId}}).sort({createdAt:-1})
-    res.status(200).json({response:postList});
-    }
-  catch(error){
+        let mytags = []
+        if(!req.body.userName)
+        throw new Error("Please send your user name!") 
+        fs.readFile('user.json', 'utf8', function readFileCallback(err, userList){
+            if (err){
+                console.log(err);
+            } else {
+            if(userList)
+            obj = JSON.parse(userList);
+            console.log("userList",userList)
+                if(!userList.includes(req.body.userName) && userList.includes("@"+req.body.userName)){
+                    res.status(403).json({error:"Inavalid user!"});
+                    return
+                }
+                else{
+                    fs.readFile('post.json', 'utf8', function readFileCallback(err, postList){
+                        if (err){
+                            console.log(err);
+                        } else {
+                        if(postList)
+                        obj = JSON.parse(postList);
+                        for( let list of obj){
+                            if(list.tagedUser.includes(req.body.userName) || userList.includes("@"+req.body.userName))
+                            mytags.push(list)
+                        }
+                        res.status(200).json({response:mytags});
+                        }});
+                    }
+                }
+            });     
+        }       
+    catch(error){
         res.status(403).json({error:error.message});
     }  
 })
